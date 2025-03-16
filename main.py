5f2ea04435997e9496f591109ad407d5529007e0
JJarvenpaa <23655632+JJarvenpaa@users.noreply.github.com>
2025-03-16 12:06:43 +0200

    Count the number of words in the book

--- main.py
+++ main.py
@@ -6,10 +6,17 @@ def get_book_text(path):
     
     return file_content;
 
+def count_words(text):
+    words = text.split();
+
+    return len(words);
+
 
 def main():
-    book_text = get_book_text('./books/frankenstein.txt');
-    print(book_text);
+    book = get_book_text('./books/frankenstein.txt');
+    num_words = count_words(book);
+    
+    print(f'{num_words} words found in the document');
 
 main();
 
