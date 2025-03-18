9c66b3fda4571d1c1cae9635ae6c85e38840e453
JJarvenpaa <23655632+JJarvenpaa@users.noreply.github.com>
2025-03-18 17:57:36 +0200

    Take book file from CLI arguments

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import sys;
+
 from stats import count_words, get_book_text, char_count, sort_chars, print_report;
 
 
 def main():
-    book_file = './books/frankenstein.txt';
+    if(len(sys.argv) < 2):
+        print('Usage: python3 main.py <book_file_name_no_extension>');
+        sys.exit(1);
+    
+    book_file = './books/' + sys.argv[1] + '.txt';
     book = get_book_text(book_file);
     num_words = count_words(book);
 
